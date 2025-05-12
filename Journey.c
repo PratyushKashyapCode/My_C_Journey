e5acf2a768d25d4216d2ce8c7447e347cd2748ea
Pratyush Kashyap <pratyushkashyap369@gmail.com>
2025-05-12 23:28:57 +0530

    Add files via upload

---
+++ Journey.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main( )
+{
+    printf("In the journey of life, many paths appear:\n");
+    printf("\tOne path leads to the wisdom of the past:\r'Question everything.'\n");
+
+    printf("\tAnother path emphasizes the quest for truth:\n");
+    printf("\t\"To be or not to be ?\"\b\b\bIs it really a question?\n");
+
+    printf("\nThe journey is filled with contradictions:\f'Can truth be false?'\n");
+
+    printf("\nAs you venture further:\n");
+    printf("\t\"The only true knowledge is in knowing you know nothing...\"\n");
+
+    printf("\nAnd so, in the end, we learn: Life is like a code:\n");
+    printf("\tWith its imperfections and mysteries,\n");
+    printf("\tIt is a journey, not a destination.\n");
+
+    return 0;
+}
