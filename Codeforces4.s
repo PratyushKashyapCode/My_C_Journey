1508fbc882c74661109824cfc2d94856522ccd9b
Pratyush Kashyap <pratyushkashyap369@gmail.com>
2025-05-27 15:31:58 +0530

    Add files via upload

---
+++ Codeforces4.s
@@ -0,0 +1,176 @@
+	.file	"Codeforces4.cpp"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB2239:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$88, %rsp
+	.seh_stackalloc	88
+	leaq	80(%rsp), %rbp
+	.seh_setframe	%rbp, 80
+	.seh_endprologue
+	call	__main
+	leaq	-4(%rbp), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt3cin(%rip), %rax
+	movq	%rax, %rcx
+.LEHB0:
+	call	_ZNSirsERi
+.LEHE0:
+	jmp	.L2
+.L5:
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1Ev
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt3cin(%rip), %rax
+	movq	%rax, %rcx
+.LEHB1:
+	call	_ZStrsIcSt11char_traitsIcESaIcEERSt13basic_istreamIT_T0_ES7_RNSt7__cxx1112basic_stringIS4_S5_T1_EE
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE6lengthEv
+	cmpq	$10, %rax
+	setbe	%al
+	testb	%al, %al
+	je	.L3
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsIcSt11char_traitsIcESaIcEERSt13basic_ostreamIT_T0_ES7_RKNSt7__cxx1112basic_stringIS4_S5_T1_EE
+	movq	%rax, %rcx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	jmp	.L4
+.L3:
+	leaq	-48(%rbp), %rax
+	movl	$0, %edx
+	movq	%rax, %rcx
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEixEy
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	movq	%rax, %rbx
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE6lengthEv
+	subq	$2, %rax
+	movq	%rax, %rdx
+	movq	%rbx, %rcx
+	call	_ZNSolsEy
+	movq	%rax, %rbx
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE6lengthEv
+	leaq	-1(%rax), %rdx
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEixEy
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edx
+	movq	%rbx, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c
+	movq	%rax, %rcx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+.LEHE1:
+.L4:
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev
+.L2:
+	movl	-4(%rbp), %eax
+	leal	-1(%rax), %edx
+	movl	%edx, -4(%rbp)
+	testl	%eax, %eax
+	setne	%al
+	testb	%al, %al
+	jne	.L5
+	movl	$0, %eax
+	jmp	.L9
+.L8:
+	movq	%rax, %rbx
+	leaq	-48(%rbp), %rax
+	movq	%rax, %rcx
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev
+	movq	%rbx, %rax
+	movq	%rax, %rcx
+.LEHB2:
+	call	_Unwind_Resume
+.LEHE2:
+.L9:
+	addq	$88, %rsp
+	popq	%rbx
+	popq	%rbp
+	ret
+	.seh_handler	__gxx_personality_seh0, @unwind, @except
+	.seh_handlerdata
+.LLSDA2239:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 .LLSDACSE2239-.LLSDACSB2239
+.LLSDACSB2239:
+	.uleb128 .LEHB0-.LFB2239
+	.uleb128 .LEHE0-.LEHB0
+	.uleb128 0
+	.uleb128 0
+	.uleb128 .LEHB1-.LFB2239
+	.uleb128 .LEHE1-.LEHB1
+	.uleb128 .L8-.LFB2239
+	.uleb128 0
+	.uleb128 .LEHB2-.LFB2239
+	.uleb128 .LEHE2-.LEHB2
+	.uleb128 0
+	.uleb128 0
+.LLSDACSE2239:
+	.text
+	.seh_endproc
+	.section .rdata,"dr"
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.def	__gxx_personality_seh0;	.scl	2;	.type	32;	.endef
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (MinGW-W64 x86_64-ucrt-posix-seh, built by Brecht Sanders, r3) 14.2.0"
+	.def	_ZNSirsERi;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZStrsIcSt11char_traitsIcESaIcEERSt13basic_istreamIT_T0_ES7_RNSt7__cxx1112basic_stringIS4_S5_T1_EE;	.scl	2;	.type	32;	.endef
+	.def	_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE6lengthEv;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsIcSt11char_traitsIcESaIcEERSt13basic_ostreamIT_T0_ES7_RKNSt7__cxx1112basic_stringIS4_S5_T1_EE;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEixEy;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c;	.scl	2;	.type	32;	.endef
+	.def	_ZNSolsEy;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev;	.scl	2;	.type	32;	.endef
+	.def	_Unwind_Resume;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
+	.section	.rdata$.refptr._ZSt3cin, "dr"
+	.globl	.refptr._ZSt3cin
+	.linkonce	discard
+.refptr._ZSt3cin:
+	.quad	_ZSt3cin
